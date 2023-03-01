7f8c29a155026dffb651ea2060d4fc18237b9988
AbulrhamnMohamed <126196441+AbulrhamnMohamed@users.noreply.github.com>
2023-03-01 18:43:16 +0200

    create model

---
+++ lib/models/new_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NoteMOdel {
+  final String title;
+  final String subtitle;
+  final String data;
+  final int color;
+
+  NoteMOdel(
+      {required this.title,
+      required this.subtitle,
+      required this.data,
+      required this.color});
+}
